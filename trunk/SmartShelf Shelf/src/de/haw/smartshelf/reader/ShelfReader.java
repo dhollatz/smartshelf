2d114487369fab4a16c4f2c8d0f6d75b31fc9467
stefan.meissner <stefan.meissner@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2007-10-28 19:45:50 +0000

    tags created, nothing interesting

--- trunk/SmartShelf Shelf/src/de/haw/smartshelf/reader/ShelfReader.java
+++ trunk/SmartShelf Shelf/src/de/haw/smartshelf/reader/ShelfReader.java
@@ -1,5 +1,10 @@
 package de.haw.smartshelf.reader;
 
+import java.util.Collection;
+
+import de.haw.smartshelf.reader.tags.RFIDTag;
+
 public interface ShelfReader {
 	// Specification of a (RFID-)Reader needed
+	public abstract Collection<RFIDTag> gatherTags();
 }
--- trunk/SmartShelf Shelf/src/de/haw/smartshelf/reader/mock/ICodeReaderMock.java
+++ trunk/SmartShelf Shelf/src/de/haw/smartshelf/reader/mock/ICodeReaderMock.java
@@ -1,7 +1,20 @@
 package de.haw.smartshelf.reader.mock;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import de.haw.smartshelf.reader.ShelfReader;
+import de.haw.smartshelf.reader.tags.ICodeTag;
+import de.haw.smartshelf.reader.tags.RFIDTag;
 
 public class ICodeReaderMock implements ShelfReader {
+
+	public Collection<RFIDTag> gatherTags() {
+		Collection<RFIDTag> tags = new ArrayList<RFIDTag>();
+		RFIDTag tag = new ICodeTag();
+		tag.setId("12345");
+		tags.add(tag);
+		return tags;
+	}
 
 }
