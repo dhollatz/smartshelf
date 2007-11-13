1e74542e9aae478786ce7c11e2d084300eeac6a7
jaroslaw.urich <jaroslaw.urich@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2007-11-13 21:00:01 +0000

    initial import

---
+++ SmartShelf Server/src/de/haw/smartshelf/server/mainpage/WellcomePage.java
@@ -0,0 +1,11 @@
+package de.haw.smartshelf.server.mainpage;
+
+
+public class WellcomePage extends MainPage
+{
+	public WellcomePage()
+	{
+		super();
+		setPageTitle("SmartShelf Wellcome Page");
+	}
+}
