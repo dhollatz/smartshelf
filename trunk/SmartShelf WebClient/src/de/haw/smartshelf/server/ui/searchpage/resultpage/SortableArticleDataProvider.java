4102d8e1b209f4518524084f157e2bfd85b56e6b
jaroslaw.urich <jaroslaw.urich@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2007-11-16 16:53:59 +0000

    added sort of columns

--- trunk/SmartShelf WebClient/src/de/haw/smartshelf/server/ui/searchpage/resultpage/SortableArticleDataProvider.java
+++ trunk/SmartShelf WebClient/src/de/haw/smartshelf/server/ui/searchpage/resultpage/SortableArticleDataProvider.java
@@ -1,5 +1,8 @@
 package de.haw.smartshelf.server.ui.searchpage.resultpage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,6 +17,54 @@ public class SortableArticleDataProvider extends SortableDataProvider
 	private static final long serialVersionUID = 1231296502023976453L;
 
 	private IArticlesHolder _articlesHolder;
+	
+	private static final Comparator<Article> _rfidAscComparator;
+	private static final Comparator<Article> _rfidDescComparator;
+	
+	private static final Comparator<Article> _articleTypeAscComparator;
+	private static final Comparator<Article> _articleTypeDescComparator;
+	
+	static
+	{
+		_rfidAscComparator = new Comparator<Article>()
+		{
+			public int compare(Article artcl1, Article artcl2)
+			{
+				return artcl1.getRfid().compareTo(artcl2.getRfid());
+			}
+
+		};
+		
+		_rfidDescComparator = new Comparator<Article>()
+		{
+			public int compare(Article artcl1, Article artcl2)
+			{
+				return artcl2.getRfid().compareTo(artcl1.getRfid());
+			}
+
+		};
+		
+		_articleTypeAscComparator = new Comparator<Article>()
+		{
+			public int compare(Article artcl1, Article artcl2)
+			{
+				return artcl1.getArticleType().compareTo(artcl2.getArticleType());
+			}
+
+		};
+		
+		_articleTypeDescComparator = new Comparator<Article>()
+		{
+			public int compare(Article artcl1, Article artcl2)
+			{
+				return artcl2.getArticleType().compareTo(artcl1.getArticleType());
+			}
+
+		};
+		
+		
+	}
+	
 
 	/**
 	 * constructor
@@ -21,7 +72,8 @@ public class SortableArticleDataProvider extends SortableDataProvider
 	public SortableArticleDataProvider(IArticlesHolder articlesHolder)
 	{
 		_articlesHolder = articlesHolder;
-		// set default sort
+
+		setSort("rfid", true);
 		setSort("articleType", true);
 	}
 
@@ -36,8 +88,37 @@ public class SortableArticleDataProvider extends SortableDataProvider
 	 */
 	public Iterator iterator(int first, int count)
 	{
+		return sort(first, count).iterator();
+	}
+
+	private List<Article> sort(int first, int count)
+	{
 		SortParam sp = getSort();
-		return getArticles().iterator();
+		String prop = sp.getProperty();
+		boolean asc = sp.isAscending();
+		Comparator<Article> comparator = null;
+		
+		if (prop == null)
+        {
+            return getArticles();
+        }
+        if (prop.equals("rfid"))
+        {
+            comparator = (asc) ? _rfidAscComparator : _rfidDescComparator;
+        }
+        else if (prop.equals("articleType"))
+        {
+            comparator = (asc) ? _articleTypeAscComparator : _articleTypeDescComparator;
+        }
+        else
+        {
+        	throw new RuntimeException("uknown sort option [" + prop +
+            "]. valid options: [rfid] , [articleType]");
+        }
+        
+        List<Article> articles = new ArrayList<Article>(getArticles());
+        Collections.sort(articles, comparator);
+        return articles;		
 	}
 
 	/**
@@ -62,5 +143,4 @@ public class SortableArticleDataProvider extends SortableDataProvider
 	{
 		return new DetachableArticleModel((Article) object);
 	}
-
 }
