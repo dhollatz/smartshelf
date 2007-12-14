ffd699ad5d7590da75157df7959216d02ad1dfce
dennis.hollatz <dennis.hollatz@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2007-12-14 18:03:06 +0000

    JNI-Primer

--- trunk/SmartShelf Testing/src/de/haw/smartshelf/reader/test/ICodeReaderTest.java
+++ trunk/SmartShelf Testing/src/de/haw/smartshelf/reader/test/ICodeReaderTest.java
@@ -1,13 +1,31 @@
 package de.haw.smartshelf.reader.test;
 
+/**
+ * 
+ * Note: SWIG Commands for wrapper generation.
+ * <code>
+ * > swig.exe -java reader.i
+ * > gcc.exe -c reader_wrap.c -IC:\Java\jdk1.5.0\include -IC:\Java\jdk1.5.0\include\win32
+ * > gcc.exe -shared reader_wrap.o -mno-cygwin -Wl,--add-stdcall-alias  -o reader.dll
+ * </code>
+ *  
+ * @author hollat_d
+ *
+ */
 public class ICodeReaderTest {
+
+	//TODO Use generated wrapper
 	static {
-		System.loadLibrary("SL2SER");
+		String path = System.getProperty("java.library.path");
+		System.out.println(path);
+//		System.loadLibrary("SL2SER");
+		System.loadLibrary("reader");
+		
 	}
 
 	public static native int CRM_init(String comStr, String baudStr);
-	
+
 	public static void main(String args[]){
-		CRM_init("COM1", "9600"); // just guessed ;)
+		CRM_init("COM1", "115200"); // just guessed ;)
 	}
 }
