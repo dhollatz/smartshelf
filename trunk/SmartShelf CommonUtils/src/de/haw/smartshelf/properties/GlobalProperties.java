f26f0c1d55b2e7d487e105e10c6993616d7d5e5a
dennis.hollatz <dennis.hollatz@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2007-12-18 16:54:39 +0000

    JNI-Primer

---
+++ trunk/SmartShelf CommonUtils/src/de/haw/smartshelf/properties/GlobalProperties.java
@@ -0,0 +1,33 @@
+/*
+ * $HeadURL: GlobalProperties.java $
+ *
+ * $Author: Dennis Hollatz $
+ * $Date: 18.12.2007 16:04:40 $
+ *
+ * Copyright 2007 by SmartShelf,
+ * Hamburg, Germany.
+ * All rights reserved.
+ */
+package de.haw.smartshelf.properties;
+
+/**
+ * This class ... Copyright (c) 2007 SmartShelf
+ *  // TODO Mechanismus zum Auslesen aus Properties oder Kommandozeile oder //
+ * so... :)
+ * 
+ * @version $ Date: 18.12.2007 16:04:40 $
+ * @author <a
+ *         href="mailto:dennis.hollatz@googlemail.com">dennis.hollatz@googlemail.com"</a>
+ */
+public class GlobalProperties
+{
+	public static String getEventHeapURL()
+	{
+		return "localhost";
+	}
+
+	public static String getClientName()
+	{
+		return "Carl";
+	}
+}
