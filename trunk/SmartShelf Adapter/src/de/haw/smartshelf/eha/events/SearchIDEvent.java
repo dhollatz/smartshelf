ccce54cf3a11b35e418b0f5f1282452ca2774829
jaroslaw.urich <jaroslaw.urich@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2008-01-08 14:52:13 +0000

    renamed events

--- trunk/SmartShelf Adapter/src/de/haw/smartshelf/eha/events/SearchIDEvent.java
+++ trunk/SmartShelf Adapter/src/de/haw/smartshelf/eha/events/SearchIDEvent.java
@@ -14,23 +14,26 @@ import iwork.eheap2.Event;
 import iwork.eheap2.EventHeapException;
 import iwork.eheap2.FieldValueTypes;
 
-public class FoundIDEventTemplate extends Event {
+public class SearchIDEvent extends Event {
 	
 	public static final String FIELD_TYPE     = "type";
 	public static final String FIELD_TITLE    = "title";
-	public static final String FIELD_LIST_XML = "listXML";
+	public static final String FIELD_TAG_ID   = "tagID";
+	public static final String FIELD_SHELF_ID = "shelfID";
+	public static final String FIELD_RACK_ID  = "rackID";
 	
-	public FoundIDEventTemplate() throws EventHeapException {
-		super("ItemList");
+	public SearchIDEvent() throws EventHeapException {
+		super("FoundIDEvent");
 		this.addField(FIELD_TYPE,     String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
 		this.addField(FIELD_TITLE,    String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
-		this.addField(FIELD_LIST_XML, String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		this.addField(FIELD_SHELF_ID, String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		this.addField(FIELD_RACK_ID,  String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
 	}
 	
 	public String getType() throws EventHeapException {
 		return this.getPostValueString(FIELD_TYPE);
 	}
-		
+	
 	public void setType(String type) throws EventHeapException {
 		this.setFieldValue(FIELD_TYPE, type);
 	}
@@ -42,13 +45,21 @@ public class FoundIDEventTemplate extends Event {
 	public void setTitle(String title) throws EventHeapException {
 		this.setFieldValue(FIELD_TITLE, title);
 	}
-	
-	public String getListXML() throws EventHeapException {
-		return this.getPostValueString(FIELD_LIST_XML);
+
+	public String getShelfID() throws EventHeapException {
+		return this.getPostValueString(FIELD_SHELF_ID);
 	}
 	
-	public void setListXML(String listXML) throws EventHeapException {
-		this.setFieldValue(FIELD_LIST_XML, listXML);
+	public void setShelfID(String shelfID) throws EventHeapException {
+		this.setFieldValue(FIELD_SHELF_ID, shelfID);
+	}
+
+	public String getRackID() throws EventHeapException {
+		return this.getPostValueString(FIELD_RACK_ID);
+	}
+	
+	public void setRackID(String rackID) throws EventHeapException {
+		this.setFieldValue(FIELD_RACK_ID, rackID);
 	}
 
 }
