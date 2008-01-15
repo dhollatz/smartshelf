c71b1380f4d20a63f8a115368a30d88d95f71a3f
dennis.hollatz <dennis.hollatz@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2008-01-15 15:05:54 +0000

    Changed event handling! IN YOUR FACE!! ;)

---
+++ SmartShelf Adapter/src/de/haw/smartshelf/eha/events/EventFactory.java
@@ -0,0 +1,69 @@
+/*
+ * $HeadURL: EventHeapAdapter.java $
+ *
+ * $Author: Dennis Hollatz $
+ * $Date: 18.12.2007 16:04:40 $
+ *
+ * Copyright 2008 by SmartShelf,
+ * Hamburg, Germany.
+ * All rights reserved.
+ */
+package de.haw.smartshelf.eha.events;
+
+import de.haw.smartshelf.bo.Article;
+import iwork.eheap2.Event;
+import iwork.eheap2.EventHeapException;
+import iwork.eheap2.FieldValueTypes;
+
+/**
+ * Die Event Factory erzeugt <tt>Event</tt>s eines Typs. 
+ * Der Typ eines Events wird &uuml;ber das Feld <code>TYPE</code> des <tt>Event</tt>s bestimmt.
+ * 
+ * @author EventHeapServer
+ *
+ */
+public class EventFactory {
+	
+	public static Event createFoundIDEvent() throws EventHeapException {
+		Event event = new Event(FoundIDEventFacade.TYPE_NAME);
+
+		event.addField(FoundIDEventFacade.FIELD_ID, String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		event.setTimeToLive(50000);
+		
+		return event;
+	}
+	
+	public static Event createResultListEvent() throws EventHeapException {
+		Event event = new Event(ResultListEventFacade.TYPE_NAME);
+
+		event.addField(ResultListEventFacade.FIELD_TITLE,    String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		event.addField(ResultListEventFacade.FIELD_LIST_XML, String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		
+		event.setTimeToLive(50000);
+		
+		return event;
+	}
+	
+	public static Event createSearchIDEvent() throws EventHeapException {
+		Event event = new Event(SearchIDEventFacade.TYPE_NAME);
+
+		event.addField(SearchIDEventFacade.FIELD_TITLE,    String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		event.addField(SearchIDEventFacade.FIELD_SHELF_ID, String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		event.addField(SearchIDEventFacade.FIELD_RACK_ID,  String.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		
+		event.setTimeToLive(50000);
+		
+		return event;
+	}
+	
+	public static Event createSearchItemEvent() throws EventHeapException {
+		Event event = new Event(SearchItemEventFacade.TYPE_NAME);
+
+		event.addField(SearchItemEventFacade.FIELD_TITLE,    String.class,  FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		event.addField(SearchItemEventFacade.FIELD_ARTICLE,  Article.class, FieldValueTypes.FORMAL, FieldValueTypes.FORMAL);
+		
+		event.setTimeToLive(50000);
+		
+		return event;
+	}
+}
---
+++ SmartShelf Adapter/src/de/haw/smartshelf/eha/events/SearchItemEventFacade.java
@@ -0,0 +1,52 @@
+/*
+ * $HeadURL: EventHeapAdapter.java $
+ *
+ * $Author: Dennis Hollatz $
+ * $Date: 18.12.2007 16:04:40 $
+ *
+ * Copyright 2007 by SmartShelf,
+ * Hamburg, Germany.
+ * All rights reserved.
+ */
+package de.haw.smartshelf.eha.events;
+
+import de.haw.smartshelf.bo.Article;
+import iwork.eheap2.Event;
+import iwork.eheap2.EventHeapException;
+
+public class SearchItemEventFacade {
+	
+	public static final String TYPE_NAME      = "SearchItemEvent";
+	
+	public static final String FIELD_TITLE    = "title";
+	public static final String FIELD_ARTICLE  = "article";
+
+	private Event event;
+	
+	public SearchItemEventFacade(Event event) throws EventHeapException {
+		if (!event.getEventType().equals(TYPE_NAME)) {
+			throw new RuntimeException("Illegal event type:" + event.getEventType());
+		}
+		this.event = event;
+	}
+	
+	public String getTitle() throws EventHeapException {
+		return event.getPostValueString(FIELD_TITLE);
+	}
+	
+	public void setTitle(String title) throws EventHeapException {
+		event.setFieldValue(FIELD_TITLE, title);
+	}
+	
+	public String getProperty(String propertyName) throws EventHeapException {
+		return event.getPostValueString(propertyName);
+	}
+	
+	public void setProperty(String propertyName, String propertyValue) throws EventHeapException {
+		event.setFieldValue(propertyName, propertyValue);
+	}
+
+	public void setArticle(Article article) throws EventHeapException {
+		event.setFieldValue(FIELD_ARTICLE, article);
+	}
+}
