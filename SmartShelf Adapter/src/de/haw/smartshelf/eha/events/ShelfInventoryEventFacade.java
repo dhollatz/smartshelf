1bae4480f93cca1059ff3f4516b6819bba4d3269
dennis.hollatz <dennis.hollatz@2a318e21-7d3d-0410-93bc-9f4b35faa153>
2008-01-30 15:57:58 +0000

    Added RemoteGUI for Displaying SmartShelf on REmote machines.

---
+++ SmartShelf Adapter/src/de/haw/smartshelf/eha/events/ShelfInventoryEventFacade.java
@@ -0,0 +1,59 @@
+package de.haw.smartshelf.eha.events;
+
+import iwork.eheap2.Event;
+import iwork.eheap2.EventHeapException;
+
+import java.io.Serializable;
+import java.util.List;
+
+//import de.haw.smartshelf.reader.tags.RFIDTag;
+
+
+public class ShelfInventoryEventFacade extends SimpleEventFacade {
+	
+	
+	public static final String TYPE_NAME       = "shelfInventoryEvent"; 
+	public static final String FIELD_SHELF_ID  = "shelfID";
+	public static final String FIELD_RFID_TAGS = "RFIDTags";
+
+	
+	public ShelfInventoryEventFacade(Event event) throws EventHeapException {
+		super(event);
+		if (!event.getEventType().equals(TYPE_NAME)) {
+			throw new RuntimeException("Illegal event type:" + event.getEventType());
+		}
+	}
+	
+	public void setShelfID(String iD) throws EventHeapException {
+		getEvent().setFieldValue(FIELD_SHELF_ID, iD);
+	}
+	
+	public Object getShelfID() throws EventHeapException {
+		return getEvent().getPostValue(FIELD_SHELF_ID);
+	}
+	
+	/**
+	 * Returns a List of RFIDTags.
+	 * (de.haw.smartshelf.reader.tags.RFIDTag)
+	 * 
+	 * @param tags
+	 * @throws EventHeapException
+	 */
+	public void setRFIDTags(List tags) throws EventHeapException {
+		getEvent().setFieldValue(FIELD_RFID_TAGS, (Serializable) tags);
+	}
+	
+	
+	/**
+	 * Returns a List of RFIDTags.
+	 * (de.haw.smartshelf.reader.tags.RFIDTag)
+	 * 
+	 * @return de.haw.smartshelf.reader.tags.RFIDTag
+	 * @throws EventHeapException
+	 */
+	@SuppressWarnings("unchecked")
+	public List getRFIDTags() throws EventHeapException {
+		return (List) getEvent().getPostValue(FIELD_RFID_TAGS);
+	}
+
+}
